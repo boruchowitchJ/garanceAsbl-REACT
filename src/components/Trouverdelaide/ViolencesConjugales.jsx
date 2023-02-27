a9173f6752d5c3a004658298d234ebc938755e20
Thais Sablon <sablon.thais@hotmail.com>
2023-02-27 18:10:17 +0100

    ajout_targetblank+noreferrer

--- src/components/Trouverdelaide/ViolencesConjugales.jsx
+++ src/components/Trouverdelaide/ViolencesConjugales.jsx
@@ -91,54 +91,54 @@ const ViolencesConjugales = () => {
 
                 <h3>Discrimination</h3>
 
-                <p>Sur base du genre : <a className="strong-yellow-exept" href="https://igvm-iefh.belgium.be/nl">Institut pour l’Egalité des femmes et des hommes</a></p>
-                <p>Pour tout autre raison : <a className="strong-yellow-exept" href="https://www.unia.be/fr">Centre pour l’Egalité des chances et la lutte contre le racisme</a></p>
-                <p>Pour racisme : <a className="strong-yellow-exept" href="https://mrax.be/"></a>Mouvement contre le racisme, l’antisémitisme et la xenophobie.</p>
+                <p>Sur base du genre : <a className="strong-yellow-exept" href="https://igvm-iefh.belgium.be/nl" target="_blank" rel="noreferrer">Institut pour l’Egalité des femmes et des hommes</a></p>
+                <p>Pour tout autre raison : <a className="strong-yellow-exept" href="https://www.unia.be/fr" target="_blank" rel="noreferrer">Centre pour l’Egalité des chances et la lutte contre le racisme</a></p>
+                <p>Pour racisme : <a className="strong-yellow-exept" href="https://mrax.be/" target="_blank" rel="noreferrer">Mouvement contre le racisme, l’antisémitisme et la xenophobie.</a></p>
 
                 <h3>Mutilations génitales et mariage forcé.</h3>
 
                 <p><a className="strong-yellow-exept" href="https://www.gams.be/">GAMS Belgique</a></p>
-                <p><a className="strong-yellow-exept" href="https://www.intact-association.org/fr/">Intact asbl</a></p>
+                <p><a className="strong-yellow-exept" href="https://www.intact-association.org/fr/" target="_blank" rel="noreferrer">Intact asbl</a></p>
 
                 <h3>Violences faites aux enfants</h3>
 
-                <p><a className="strong-yellow-exept" href="https://childfocus.be/fr-be/">CHILD FOCUS</a></p>
-                <p><a className="strong-yellow-exept" href="http://www.federationsosenfants.be/">Equipes SOS Enfants</a></p>
-                <p><a className="strong-yellow-exept" href="https://www.asblkaleidos.be/">Kaleidos asbl</a></p>
+                <p><a className="strong-yellow-exept" href="https://childfocus.be/fr-be/" target="_blank" rel="noreferrer">CHILD FOCUS</a></p>
+                <p><a className="strong-yellow-exept" href="http://www.federationsosenfants.be/" target="_blank" rel="noreferrer">Equipes SOS Enfants</a></p>
+                <p><a className="strong-yellow-exept" href="https://www.asblkaleidos.be/" target="_blank" rel="noreferrer">Kaleidos asbl</a></p>
 
                 <h3>Violences faites aux femmes lesbiennes, bisexuelles</h3>
 
-                <p><a className="strong-yellow-exept" href="http://rainbowhouse.be/fr/">Rainbow House</a></p>
+                <p><a className="strong-yellow-exept" href="http://rainbowhouse.be/fr/" target="_blank" rel="noreferrer">Rainbow House</a></p>
 
                 <h3>Violences faites aux personnes âgées</h3>
 
-                <p><a className="strong-yellow-exept" href="http://www.respectseniors.be/">Respect Séniors</a> , Wallonie.</p>
-                <p><a className="strong-yellow-exept" href="http://www.rifvel.be/">Viellir en liberté.</a></p>
+                <p><a className="strong-yellow-exept" href="http://www.respectseniors.be/" target="_blank" rel="noreferrer">Respect Séniors</a> , Wallonie.</p>
+                <p><a className="strong-yellow-exept" href="http://www.rifvel.be/" target="_blank" rel="noreferrer">Viellir en liberté.</a></p>
 
                 <h3>Violences faites aux personnes aux genres fluides, trans et intersexes</h3>
 
-                <p><a className="strong-yellow-exept" href="https://www.genrespluriels.be/">Genres Pluriels.</a></p>
+                <p><a className="strong-yellow-exept" href="https://www.genrespluriels.be/" target="_blank" rel="noreferrer">Genres Pluriels.</a></p>
 
                 <h3>Violences faites aux personnes prostituées</h3>
 
-                <p><a className="strong-yellow-exept" href="https://www.alias-bru.be/">Alias</a> , prostitutuion masculine.</p>
-                <p><a className="strong-yellow-exept" href="https://espacep.be/">Espace P...</a></p>
-                <p><a className="strong-yellow-exept" href="https://pag-asa.be/">Pag-Asa</a>, traite des êtres humains.</p>
+                <p><a className="strong-yellow-exept" href="https://www.alias-bru.be/" target="_blank" rel="noreferrer">Alias</a> , prostitutuion masculine.</p>
+                <p><a className="strong-yellow-exept" href="https://espacep.be/" target="_blank" rel="noreferrer">Espace P...</a></p>
+                <p><a className="strong-yellow-exept" href="https://pag-asa.be/" target="_blank" rel="noreferrer">Pag-Asa</a>, traite des êtres humains.</p>
             </div>
             <div className="violence-autres-autres">
                 <div className="redBox">
                     <h3>France</h3>
                     
-                    <p><a className="strong-yellow-exept" href="http://www.sosfemmes.com/" target="_blank">SOS Femmes</a> - annuaire de services d’aide pour femmes</p> 
+                    <p><a className="strong-yellow-exept" href="http://www.sosfemmes.com/" target="_blank" rel="noreferrer">SOS Femmes</a> - annuaire de services d’aide pour femmes</p> 
 
                     <h4>Aide aux victimes</h4>
-                    <p><a className="strong-yellow-exept" href="https://www.france-victimes.fr/" target="_blank">CInstitut national d’aide aux victimes et de médiation.</a> - liste de services par département</p>
+                    <p><a className="strong-yellow-exept" href="https://www.france-victimes.fr/" target="_blank" rel="noreferrer">Institut national d’aide aux victimes et de médiation.</a> - liste de services par département</p>
 
                     <h4>Discrimination</h4>
-                    <p><a className="strong-yellow-exept" href="https://www.lacimade.org/" target="_blank">CIMADE</a></p> 
-                    <p><a className="strong-yellow-exept" href="https://www.defenseurdesdroits.fr/fr/institution/competences/lutte-contre-discriminations" target="_blank">HALDE.</a></p>
-                    <p><a className="strong-yellow-exept" href="https://www.sos-homophobie.org/" target="_blank">SOS Homophobie.</a></p>
-                    <p><a className="strong-yellow-exept" href="https://www.sos-homophobie.org/" target="_blank">SOS Racisme.</a></p>
+                    <p><a className="strong-yellow-exept" href="https://www.lacimade.org/" target="_blank" rel="noreferrer">CIMADE</a></p> 
+                    <p><a className="strong-yellow-exept" href="https://www.defenseurdesdroits.fr/fr/institution/competences/lutte-contre-discriminations" target="_blank" rel="noreferrer">HALDE.</a></p>
+                    <p><a className="strong-yellow-exept" href="https://www.sos-homophobie.org/" target="_blank" rel="noreferrer">SOS Homophobie.</a></p>
+                    <p><a className="strong-yellow-exept" href="https://www.sos-homophobie.org/" target="_blank" rel="noreferrer">SOS Racisme.</a></p>
                     
                     <h4>Harcèlement et violences au travail</h4>
 
@@ -147,12 +147,12 @@ const ViolencesConjugales = () => {
                 <div className="redBox">
                     <h3>Suisse</h3>
                     
-                    <a className="strong-yellow-exept" href="https://www.opferhilfe-schweiz.ch/fr/" target="_blank">Centres de consultation LAVI</a>
+                    <a className="strong-yellow-exept" href="https://www.opferhilfe-schweiz.ch/fr/" target="_blank" rel="noreferrer">Centres de consultation LAVI</a>
                     <h4>Harcèlement au travail</h4>
-                    <a className="strong-yellow-exept" href="https://www.non-c-non.ch/" target="_blank">Non c'est non.</a>
+                    <a className="strong-yellow-exept" href="https://www.non-c-non.ch/" target="_blank" rel="noreferrer">Non c'est non.</a>
                     <h4>Violences faites aux enfants</h4>
-                    <p><a className="strong-yellow-exept" href="https://www.disno.ch/">Dis No.</a></p>
-                    <p><a className="strong-yellow-exept" href="http://www.fjfnet.ch/Violence.php">Violence et Famille.</a></p>
+                    <p><a className="strong-yellow-exept" href="https://www.disno.ch/" target="_blank" rel="noreferrer">Dis No.</a></p>
+                    <p><a className="strong-yellow-exept" href="http://www.fjfnet.ch/Violence.php" target="_blank" rel="noreferrer">Violence et Famille.</a></p>
                 </div> 
                 <div className="redBox">
                     <h3>Canada</h3>
