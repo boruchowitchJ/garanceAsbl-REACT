66de98aed87594131e23ed6e1cdd534066141acc
Thais Sablon <sablon.thais@hotmail.com>
2023-04-29 21:15:06 +0200

    TrouverAide_Ongoing

--- src/components/Trouverdelaide/TrouverAide.jsx
+++ src/components/Trouverdelaide/TrouverAide.jsx
@@ -1,31 +1,81 @@
-
-import ViolencesConjugales from './ViolencesConjugales';
-import ViolencesSexuelles from './ViolencesSexuelles';
+/* import ViolencesConjugales from "./ViolencesConjugales";
+import ViolencesSexuelles from "./ViolencesSexuelles";
 import AutresViolences from "./AutresTypesViolences";
-import AidePsychoSociale from "./AidePsychoSociale";
+import AidePsychoSociale from "./AidePsychoSociale"; */
 
-import './Trouverdelaide.css';
+import ScrollAnimation from "react-animate-on-scroll";
+import TrouverAideCard from "./TrouverAideCard";
 
+import "./Trouverdelaide.css";
+
+const TrouverAideTab = [
+  {
+    id: 1,
+    titre: "Violences Conjugales",
+    description: "Où trouver de l'aide en cas de violences conjugales.",
+    affiche: "../../assets/images/aidePsychoSociale.jpg",
+    lien: "/echappesbelles#echappe",
+  },
+  {
+    id: 2,
+    titre: "Violences Sexuelles",
+    description: "Où trouver de l'aide en cas de violences sexuelles",
+    affiche: "../../assets/images/aidePsychoSociale.jpg",
+    lien: "/GaranceFemme#femme",
+  },
+  {
+    id: 3,
+    titre: "Tout autre type de violence",
+    description: "Où trouver de l'aide en cas de tout autre type de violence.",
+    affiche: "../../assets/images/aidePsychoSociale.jpg",
+    lien: "/GaranceHomme#homme",
+  },
+  {
+    id: 4,
+    titre: "Sites d'aide psychosociale",
+    description: "Répertoire de site d'aide psycho-sociale.",
+    affiche: "../../assets/images/aidePsychoSociale.jpg",
+    lien: "/Garance55#55",
+  },
+];
 const TrouverAide = () => {
-
-    return(
-        <div className='container-trouver-aide' id='touverdelaide'>
-            <div>
-                <ViolencesConjugales />
+  return (
+    <div
+      className="container-trouver-aide"
+      id="touverdelaide"
+      style={{ paddingTop: "40%" }}
+    >
+      <div>
+        <ScrollAnimation animateIn="fadeInLeft">
+          <div class="container" style={{ marginTop: "-30%", height: "auto" }}>
+            <div class="row">
+              {TrouverAideTab.map((p) => (
+                <TrouverAideCard
+                  key={p.id}
+                  titre={p.titre}
+                  description={p.description}
+                  affiche={p.affiche}
+                  lien={p.lien}
+                />
+              ))}
             </div>
-            <div>
-                <ViolencesSexuelles />
-            </div>
-            <div>
-                <AutresViolences />
-            </div>
-            <div>
-                <AidePsychoSociale />
-            </div>
-        </div>
-
-        
-    )
+          </div>
+        </ScrollAnimation>
+      </div>
+      {/* <div>
+        <ViolencesConjugales />
+      </div>
+      <div>
+        <ViolencesSexuelles />
+      </div>
+      <div>
+        <AutresViolences />
+      </div>
+      <div>
+        <AidePsychoSociale />
+      </div> */}
+    </div>
+  );
 };
 
-export default TrouverAide ;
+export default TrouverAide;
---
+++ src/components/Trouverdelaide/TrouverAideCard.jsx
@@ -0,0 +1,45 @@
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { HashLink } from "react-router-hash-link";
+
+const TrouverAideCard = (props) => {
+  const { titre, description, affiche, lien } = props;
+
+  return (
+    <section class="col-md-6 my-5">
+      <main>
+        <div>
+          <div
+            class="card text-center"
+            style={{
+              height: "450px",
+              background: "hsla(0, 0%, 100%, 0.55)",
+              backdropfilter: "blur(30px)",
+            }}
+          >
+            <div
+              class="bg-image hover-overlay ripple"
+              data-mdb-ripple-color="light"
+            >
+              <img src={affiche} class="img-fluid" />
+              <a href="#!">
+                <div
+                  class="mask"
+                  style={{ backgroundcolor: "rgba(251, 251, 251, 0.15)" }}
+                ></div>
+              </a>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">{titre}</h5>
+              <p class="card-text">{description}</p>
+              <HashLink to={lien} class="btn link">
+                En savoir plus
+              </HashLink>
+            </div>
+          </div>
+        </div>
+      </main>
+    </section>
+  );
+};
+export default TrouverAideCard;
