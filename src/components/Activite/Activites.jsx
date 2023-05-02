fdb74a084adaf5be4e8ac4fa8e68c50abe08b167
Thais Sablon <sablon.thais@hotmail.com>
2023-05-02 12:15:45 +0200

    routeActivites+Cards

---
+++ src/components/Activite/Activites.jsx
@@ -0,0 +1,91 @@
+import ScrollAnimation from "react-animate-on-scroll";
+import ActivitesCard from "./ActivitesCard";
+
+import "./Activites.css";
+
+const Activites = () => {
+  const ActivitesTab1 = [
+    {
+      id: 1,
+      titre: "Résister à l'antiféminisme",
+      description:
+        "Pour toutes les femmes (selon autodéfinition) qui ne veulent plus laisser passer les blagues lourdes,les remarques...",
+      affiche: "../../assets/images/ateliers/resisterAntiFeminisme.jpg",
+      lien: "/echappesbelles#echappe",
+    },
+    {
+      id: 2,
+      titre: "Auto-défense féministe",
+      description:
+        "Pour toutes les femmes (selon autodéfinition) qui veulent agrandir leur sécurité en prévenant les agressions...",
+      affiche: "../../assets/images/ateliers/autoDefenseFeministe.jpg",
+      lien: "/GaranceFemme#femme",
+    },
+    {
+      id: 3,
+      titre: "Défense Verbale",
+      description:
+        "Pour toutes les femmes (selon autodéfinition) qui ont envie d’apprendre à poser leurs limites verbalement.",
+      affiche: "../../assets/images/ateliers/defenseVerbale.jpg",
+      lien: "/GaranceHomme#homme",
+    },
+  ];
+  const ActivitesTab2 = [
+    {
+      id: 1,
+      titre: "Auto-défense pour filles",
+      description:
+        "Les filles ne sont pas épargnées par les violences, mais elles peuvent découvrir par ces formations adaptées....",
+      affiche: "../../assets/images/ateliers/autoDefenseFilles.jpg",
+      lien: "/echappesbelles#echappe",
+    },
+    {
+      id: 2,
+      titre: "Entrainement Mensuel",
+      description:
+        "Vous avez déjà suivi une formation d’autodéfense précédemment ? Cet entraînement est fait pour vous...",
+      affiche: "../../assets/images/ateliers/entrainementMensuel.jpg",
+      lien: "/GaranceFemme#femme",
+    },
+  ];
+
+  return (
+    <div className="containerActivites">
+      <div className="cardRow">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <div class="container" style={{ marginTop: "-30%", height: "auto" }}>
+            <div class="row">
+              {ActivitesTab1.map((p) => (
+                <ActivitesCard
+                  key={p.id}
+                  titre={p.titre}
+                  description={p.description}
+                  affiche={p.affiche}
+                  lien={p.lien}
+                />
+              ))}
+            </div>
+          </div>
+        </ScrollAnimation>
+      </div>
+      <div className="cardRow">
+        <ScrollAnimation animateIn="fadeInRight">
+          <div class="container" style={{ marginTop: "-30%", height: "auto" }}>
+            <div class="row">
+              {ActivitesTab2.map((p) => (
+                <ActivitesCard
+                  key={p.id}
+                  titre={p.titre}
+                  description={p.description}
+                  affiche={p.affiche}
+                  lien={p.lien}
+                />
+              ))}
+            </div>
+          </div>
+        </ScrollAnimation>
+      </div>
+    </div>
+  );
+};
+export default Activites;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,117 +1,236 @@
-import './Navbar.css'
-import { Outlet, Link } from "react-router-dom";
-import React, { useState } from 'react';
+import "./Navbar.css";
+import { Outlet } from "react-router-dom";
+import React, { useState } from "react";
 import {
-    MDBContainer,
-    MDBNavbar,
-    MDBNavbarBrand,
-    MDBNavbarToggler,
-    MDBIcon,
-    MDBNavbarNav,
-    MDBNavbarItem,
-    MDBNavbarLink,
-    MDBBtn,
-    MDBDropdown,
-    MDBDropdownToggle,
-    MDBDropdownMenu,
-    MDBDropdownItem,
-    MDBCollapse,
-} from 'mdb-react-ui-kit';
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBCollapse,
+} from "mdb-react-ui-kit";
 
 const Nav = () => {
-    const [showBasic, setShowBasic] = useState(false);
+  const [showBasic, setShowBasic] = useState(false);
 
-    return (
-        <>
-            <header >
+  return (
+    <>
+      <header>
+        <MDBNavbar
+          style={{ zIndex: "999", fontWeight: 400, boxShadow: "none" }}
+          expand="lg"
+        >
+          <MDBContainer fluid className="fixed-top">
+            <MDBNavbarBrand style={{ marginTop: 20 }}>
+              <img
+                src="./assets/images/garancelogo.png"
+                height="150px"
+                alt=""
+                loading="lazy"
+                className=".logoNav"
+              />
+            </MDBNavbarBrand>
 
-                <MDBNavbar style={{ zIndex: "999",fontWeight:400,boxShadow:"none" }} expand='lg' >
-                    <MDBContainer fluid  className='fixed-top' >
-                        <MDBNavbarBrand style={{marginTop:20}} >
-                            <img
-                                src='./assets/images/garancelogo.png'
-                                height='150px'
-                                
-                                alt=''
-                                loading='lazy'
-                                className='.logoNav'
-                            />
-                        </MDBNavbarBrand>
+            <MDBNavbarToggler
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              onClick={() => setShowBasic(!showBasic)}
+            >
+              <MDBIcon icon="bars" fas />
+            </MDBNavbarToggler>
 
-                        <MDBNavbarToggler
-                            aria-controls='navbarSupportedContent'
-                            aria-expanded='false'
-                            aria-label='Toggle navigation'
-                            onClick={() => setShowBasic(!showBasic)}
-                        >
-                            <MDBIcon icon='bars' fas />
-                        </MDBNavbarToggler>
+            <MDBCollapse style={{ height: 80 }} navbar show={showBasic}>
+              <MDBNavbarNav
+                className="justify-content-evenly align-item-center nav-items "
+                style={{}}
+              >
+                <MDBNavbarItem>
+                  <MDBNavbarLink active aria-current="page" href="/">
+                    Accueil
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
 
-                        <MDBCollapse  style={{height: 80 }} navbar show={showBasic} > 
-                            <MDBNavbarNav className='justify-content-evenly align-item-center nav-items ' style={{ }}>
-                                <MDBNavbarItem >
-                                    <MDBNavbarLink  active aria-current='page' href='/'>
-                                        Accueil
-                                    </MDBNavbarLink>
-                                </MDBNavbarItem>
+                <MDBNavbarItem>
+                  <MDBDropdown>
+                    <MDBDropdownToggle
+                      style={{ display: "flex", alignItems: "center" }}
+                      tag="a"
+                      role="button"
+                    >
+                      <MDBNavbarLink
+                        style={{ display: "flex", alignItems: "center" }}
+                        href="/pourvous"
+                      >
+                        Pour vous
+                      </MDBNavbarLink>
+                    </MDBDropdownToggle>
+                    <MDBDropdownMenu
+                      className="sous"
+                      style={{ backgroundColor: "#f000" }}
+                    >
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/echappesbelles">
+                          Echapees Belles
+                        </MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/GaranceFemme ">
+                          Garance Femme{" "}
+                        </MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/GaranceHomme  ">
+                          Garance Homme
+                        </MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/Garance55  ">
+                          Garance +55{" "}
+                        </MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/Garance18 ">
+                          Garance -18{" "}
+                        </MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/GarancePro">
+                          Garance Pro
+                        </MDBNavbarLink>
+                      </MDBDropdownItem>
+                    </MDBDropdownMenu>
+                  </MDBDropdown>
+                </MDBNavbarItem>
+                <MDBNavbarItem>
+                  <MDBDropdown>
+                    <MDBDropdownToggle
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        width: 100,
+                      }}
+                      tag="a"
+                      className=""
+                      role="button"
+                    >
+                      <MDBNavbarLink href="/QuisommeNous">
+                        Qui sommes Nous?
+                      </MDBNavbarLink>
+                    </MDBDropdownToggle>
+                    <MDBDropdownMenu
+                      className="sous"
+                      style={{ backgroundColor: "#f000" }}
+                    >
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/Histoire">Histoire</MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/Positionnement">
+                          Positionnement
+                        </MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/Reseau">Reseau</MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/Statut">Statut</MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/Partenariat">
+                          Partenariat{" "}
+                        </MDBNavbarLink>
+                      </MDBDropdownItem>
+                    </MDBDropdownMenu>
+                  </MDBDropdown>
+                </MDBNavbarItem>
+                <MDBNavbarItem style={{ display: "flex" }}>
+                  <MDBDropdown>
+                    <MDBDropdownToggle
+                      style={{ display: "flex", alignItems: "center" }}
+                      tag="a"
+                      className=""
+                      role="button"
+                    >
+                      <MDBNavbarLink href="/Activites">Ateliers</MDBNavbarLink>
+                    </MDBDropdownToggle>
+                    <MDBDropdownMenu
+                      className="sous"
+                      style={{ backgroundColor: "#f000" }}
+                    >
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="#">Notre calendrier</MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="#">Nos ateliers</MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="#">Inscription</MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="#">Plan d'accès</MDBNavbarLink>
+                      </MDBDropdownItem>
+                    </MDBDropdownMenu>
+                  </MDBDropdown>
+                </MDBNavbarItem>
+                <MDBNavbarItem style={{ display: "flex" }}>
+                  <MDBDropdown>
+                    <MDBDropdownToggle
+                      style={{ display: "flex", alignItems: "center" }}
+                      tag="a"
+                      className=""
+                      role="button"
+                    >
+                      <MDBNavbarLink href="/TrouverAide">
+                        Trouver de l'aide
+                      </MDBNavbarLink>
+                    </MDBDropdownToggle>
 
-
-                                <MDBNavbarItem  >
-                                    <MDBDropdown  >
-                                        <MDBDropdownToggle style={{display: "flex", alignItems: "center"}} tag='a' role='button'>
-                                            <MDBNavbarLink style={{display: "flex", alignItems: "center",}} href='/pourvous'>Pour vous</MDBNavbarLink>
-                                        </MDBDropdownToggle>
-                                        <MDBDropdownMenu className='sous' style={{ backgroundColor: "#f000" }}  >
-                                            <MDBDropdownItem ><MDBNavbarLink href='/echappesbelles'>Echapees Belles</MDBNavbarLink></MDBDropdownItem>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/GaranceFemme ">Garance Femme </MDBNavbarLink></MDBDropdownItem>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/GaranceHomme  ">Garance Homme</MDBNavbarLink></MDBDropdownItem>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/Garance55  ">Garance +55 </MDBNavbarLink></MDBDropdownItem>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/Garance18 ">Garance -18 </MDBNavbarLink></MDBDropdownItem>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/GarancePro">Garance Pro</MDBNavbarLink></MDBDropdownItem>
-                                        </MDBDropdownMenu>
-                                    </MDBDropdown>
-                                </MDBNavbarItem>
-                                <MDBNavbarItem>
-                                    <MDBDropdown>
-                                        <MDBDropdownToggle style={{display: "flex", alignItems: "center",width:100}} tag='a' className='' role='button'>
-                                            <MDBNavbarLink href="/QuisommeNous">Qui sommes Nous?</MDBNavbarLink>
-                                        </MDBDropdownToggle>
-                                        <MDBDropdownMenu className='sous' style={{ backgroundColor: "#f000" }}>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/Histoire">Histoire</MDBNavbarLink></MDBDropdownItem>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/Positionnement">Positionnement</MDBNavbarLink></MDBDropdownItem>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/Reseau">Reseau</MDBNavbarLink></MDBDropdownItem>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/Statut">Statut</MDBNavbarLink></MDBDropdownItem>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/Partenariat">Partenariat </MDBNavbarLink></MDBDropdownItem>
-                                        </MDBDropdownMenu>
-                                    </MDBDropdown>
-                                </MDBNavbarItem>
-                                {/* ici mettre ton drop menu Activité */}
-                                <MDBNavbarItem style={{ display: "flex" }}>
-                                    <MDBDropdown >
-                                        <MDBDropdownToggle style={{display: "flex", alignItems: "center"}} tag='a' className='' role='button'>
-                                            <MDBNavbarLink href="/TrouverAide">Trouver de l'aide</MDBNavbarLink>
-                                        </MDBDropdownToggle>
-
-                                        <MDBDropdownMenu className='sous' style={{ backgroundColor: "#f000" }}>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/ViolencesConjugales">Violence Conjugale</MDBNavbarLink></MDBDropdownItem>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/ViolencesSexuelles">Violence sexuelle</MDBNavbarLink></MDBDropdownItem>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/AutresTypesViolences">Autres type de violence</MDBNavbarLink></MDBDropdownItem>
-                                            <MDBDropdownItem ><MDBNavbarLink href="/AidePsychoSociale">Aide psycho-sociale</MDBNavbarLink></MDBDropdownItem>
-
-                                        </MDBDropdownMenu>
-                                    </MDBDropdown>
-                                </MDBNavbarItem>
-                                <MDBNavbarItem>
-                                    <MDBNavbarLink href='/contact'>Contact</MDBNavbarLink>
-                                </MDBNavbarItem>
-                            </MDBNavbarNav>
-                        </MDBCollapse>
-                    </MDBContainer>
-                </MDBNavbar></header>
-            <Outlet /></>
-    );
-
-}
+                    <MDBDropdownMenu
+                      className="sous"
+                      style={{ backgroundColor: "#f000" }}
+                    >
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/ViolencesConjugales">
+                          Violence Conjugale
+                        </MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/ViolencesSexuelles">
+                          Violence sexuelle
+                        </MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/AutresTypesViolences">
+                          Autres type de violence
+                        </MDBNavbarLink>
+                      </MDBDropdownItem>
+                      <MDBDropdownItem>
+                        <MDBNavbarLink href="/AidePsychoSociale">
+                          Aide psycho-sociale
+                        </MDBNavbarLink>
+                      </MDBDropdownItem>
+                    </MDBDropdownMenu>
+                  </MDBDropdown>
+                </MDBNavbarItem>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/contact">Contact</MDBNavbarLink>
+                </MDBNavbarItem>
+              </MDBNavbarNav>
+            </MDBCollapse>
+          </MDBContainer>
+        </MDBNavbar>
+      </header>
+      <Outlet />
+    </>
+  );
+};
 
 export default Nav;
-
