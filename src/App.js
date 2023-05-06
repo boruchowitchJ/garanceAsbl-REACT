d95c5901a3888936de8e52739c75e541a169b6c3
boruchowitchJ <boru_jennifer@hotmail.fr>
2023-05-06 12:38:17 +0200

    Revert "navigation"
    This reverts commit f6eb7b7b31ddab404cff7087c5d48ee00875c0f3.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import "./App.css";
+import './App.css';
 
-import Router from "./Router";
+
+
+
+
+
 
 function App() {
-  return <Router />;
+  return (
+    <div className="App">
+      {/* Ajouter : npm install bootstrap */}
+
+
+      {/* IL NE FAUT NORMALEMENT RIEN ECRIRE ICI VU QUE TOUT SE FAIT DANS LE ROUTAGE ON DEMANDERA A AUDE MAIS J AI VU QUE C 44ETAIT UN ERREUR DE MA PART */}
+      
+
+      
+    </div>
+  );
 }
 
 export default App;
--- src/components/Pourvous/PourvousColl/PourVousList.jsx
+++ src/components/Pourvous/PourvousColl/PourVousList.jsx
@@ -1,94 +1,43 @@
-import PourVousCard from "./PourVousCard";
-import ScrollAnimation from "react-animate-on-scroll";
+import PourVousCard from "./PourVousCard"
+import ScrollAnimation from 'react-animate-on-scroll';
 const PourVousList = () => {
-  const pourvousliste = [
-    {
-      id: 1,
-      titre: "Echappée Belle",
-      description: "L’autodéfense est tout ce qui rend nos vies plus sûres.",
-      affiche: "../../assets/images/femme3.jpg",
-      lien: "/echappesbelles#echappe",
-    },
-    {
-      id: 2,
-      titre: "Garance Femme ",
-      description: "être plus zen dans toutes les circonstances de la vie",
-      affiche: "../../assets/images/femme.jpg ",
-      lien: "/GaranceFemme#femme",
-    },
-    {
-      id: 3,
-      titre: "Garance Homme ",
-      description:
-        "Garance mène une réflexion sur la prévention des violences chez les hommes",
-      affiche: "../../assets/images/homme.jpg ",
-      lien: "/GaranceHomme#homme",
-    },
-    {
-      id: 4,
-      titre: "Garance +55 ",
-      description: "Vous pouvez être actrice de votre sécurité",
-      affiche: "../../assets/images/55+.jpg ",
-      lien: "/Garance55#55",
-    },
-    {
-      id: 5,
-      titre: "Garance -18 ",
-      description: "Garance contribue à les rendre moins vulnérables !",
-      affiche: "../../assets/images/18.jpg ",
-      lien: "/Garance18#18",
-    },
-    {
-      id: 5,
-      titre: "Garance Pro ",
-      description: "Prevenir des violences est professionnelle incontournable",
-      affiche: "../../assets/images/pro.jpg ",
-      lien: "/GarancePro#pro",
-    },
-  ];
-
-  return (
-    <>
-      {/* <div class="hero-image-pourvous">
+    const pourvousliste = [
+        { id:1, titre:"Echappée Belle", description: "L’autodéfense est tout ce qui rend nos vies plus sûres.",  affiche:"../../assets/images/femme3.jpg", lien:"/echappesbelles#echappe"},
+        { id:2, titre:"Garance Femme ", description: "être plus zen dans toutes les circonstances de la vie", affiche:"../../assets/images/femme.jpg ", lien:"/GaranceFemme#femme" }, 
+        { id:3, titre:"Garance Homme ", description: "Garance mène une réflexion sur la prévention des violences chez les hommes", affiche:"../../assets/images/homme.jpg ", lien:"/GaranceHomme#homme" } ,
+        { id:4, titre:"Garance +55 ", description: "Vous pouvez être actrice de votre sécurité", affiche:"../../assets/images/55+.jpg ", lien:"/Garance55#55" } ,
+        { id:5, titre:"Garance -18 ", description: "Garance contribue à les rendre moins vulnérables !", affiche:"../../assets/images/18.jpg ", lien:"/Garance18#18"} ,
+        { id:5, titre:"Garance Pro ", description: "Prevenir des violences est professionnelle incontournable", affiche:"../../assets/images/pro.jpg ", lien:"/GarancePro#pro" } 
+        
+      ]
+       
+    return (
+        <>
         <div
-          class="mask"
-          style={{ backgroundcolor: "rgba(0, 0, 0, 0.0)" }}
-        ></div>
-      </div> */}
+     class="hero-image-pourvous">
+        
+  <div class="mask" style={{backgroundcolor: "rgba(0, 0, 0, 0.0)"}}></div>
+</div>
 
-      <section class="grainy-gradient-intro">
-        <svg height="0" width="0">
-          <filter id="f">
-            <feTurbulence type="fractalNoise" baseFrequency=".5" />
-          </filter>
-        </svg>
 
-        <ScrollAnimation animateIn="fadeInLeft">
-          <div
-            class="container"
-            style={{
-              display: "flex",
-              position: "relative",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
+
+<section class="grainy-gradient-intro">
+  <svg height="0" width="0">
+    <filter id='f'>
+      <feTurbulence type='fractalNoise' baseFrequency='.5' />
+    </filter>
+  </svg>
+  
+  <ScrollAnimation animateIn='fadeInLeft'>
+        <div class="container"style={{marginTop: "-30%",height:"auto" }}>
             <div class="row">
-              {pourvousliste.map((p) => (
-                <PourVousCard
-                  key={p.id}
-                  titre={p.titre}
-                  description={p.description}
-                  affiche={p.affiche}
-                  lien={p.lien}
-                />
-              ))}
-            </div>
-          </div>
-        </ScrollAnimation>
-      </section>
-    </>
-  );
-};
+           { pourvousliste.map(p => <PourVousCard key ={p.id} titre={p.titre} description={p.description} affiche={p.affiche} lien={p.lien}/>)}
+</div>
+        </div></ScrollAnimation>
+        </section>
+  
+        </>
+    )
+}
 
 export default PourVousList;
